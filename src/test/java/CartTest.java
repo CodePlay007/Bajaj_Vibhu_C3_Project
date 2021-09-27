65acb690dc95a871adb22444153b7d858549e691
Codeplay007 <bajajvibhu@hotmail.com>
2021-09-27 23:51:40 +0530

    Part 3: Failing test case

---
+++ src/test/java/CartTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import java.time.LocalTime;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartTest {
+
+
+    @Test
+    public void check_if_after_adding_item_to_cart_its_total_cart_value_returns_correct_amount(){
+
+    }
+
+    @Test
+    public void check_If_addItemToCart_Method_Adds_Items_To_Cart() {
+
+    }
+
+    @Test
+    public void checks_if_removing_item_from_cart_decrease_its_size_by_1_and_updates_cart_total_amount() {
+
+    }
+
+}
